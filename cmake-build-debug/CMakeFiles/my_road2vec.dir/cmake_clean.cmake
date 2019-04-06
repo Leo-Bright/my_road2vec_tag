4177a018192acb7fd32e5cc2c528664c77cab5e4
Leo <xinming.lai@qq.com>
2019-04-06 13:59:56 +0800

    同类型学习增加node tag

---
+++ cmake-build-debug/CMakeFiles/my_road2vec.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_road2vec.dir/main.c.o"
+  "my_road2vec.pdb"
+  "my_road2vec"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/my_road2vec.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
